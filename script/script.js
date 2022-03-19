76cd5b63a1820150b16c723b9060e159ae9d8a44
Artiom_Sokolov <sokolova-net@mail.ru>
2022-03-19 19:37:30 +0300

    btn to top disable

--- script/script.js
+++ script/script.js
@@ -3,4 +3,12 @@ let btnToTop = document.querySelector('.footer__btn-to-top');
   btnToTop.onclick = function () {
     window.scrollTo(0, 0);
   };
+
+  window.onscroll = function () {
+    if (window.pageYOffset > 200) {
+      btnToTop.classList.add('shown');
+    } else {
+      btnToTop.classList.remove('shown');
+      }
+  };
   
